4a16915605b175c1e17742292a974c2279e8374d
Stefan Stoichev <stefan.stoichev@gmail.com>
2017-10-22 18:52:38 +0000

    distribution step added on build

--- build/build.js
+++ build/build.js
@@ -1,5 +1,7 @@
 'use strict'
 require('./check-versions')()
+var client = require('scp2');
+var credentials = require('/home/ubuntu/workspace/qlik-snippets/credentials.json')
 
 process.env.NODE_ENV = 'production'
 
@@ -16,7 +18,7 @@ spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
-  webpack(webpackConfig, function (err, stats) {
+  webpack(webpackConfig, function(err, stats) {
     spinner.stop()
     if (err) throw err
     process.stdout.write(stats.toString({
@@ -37,5 +39,12 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
       '  Tip: built files are meant to be served over an HTTP server.\n' +
       '  Opening index.html over file:// won\'t work.\n'
     ))
+
+
+    client.scp(credentials.source, credentials.dest, function(err) {
+      if(err) throw err;
+      console.log('Distribute complete')
+    })
+
   })
 })
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -24,15 +24,15 @@ const webpackConfig = merge(baseWebpackConfig, {
   // devtool: config.build.productionSourceMap ? '#source-map' : false,
   output: {
     path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    filename: utils.assetsPath('js/[name].js'),
+    chunkFilename: utils.assetsPath('js/[id].js')
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
-    new BundleAnalyzerPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': env
-    }),
+    // new BundleAnalyzerPlugin(),
+    // new webpack.DefinePlugin({
+    //   'process.env': env
+    // }),
     // UglifyJs do not support ES6+, you can also use babel-minify for better treeshaking: https://github.com/babel/minify
     new webpack.optimize.UglifyJsPlugin({
       compress: {
@@ -42,7 +42,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css')
+      filename: utils.assetsPath('css/[name].css')
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
